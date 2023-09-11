11ce41580a9c02c3a369bbcb2390e9013c618f3a
egonulates <emregonulates@gmail.com>
2023-09-10 23:16:15 -0400

    Added "D2" (also known WRMSD) method to ipd() function. Improved robust-z method to better deal with polytomous items. Improved documentation and added unit tests.

--- R/ipd.R
+++ R/ipd.R
@@ -2,9 +2,22 @@
 #' Item Parameter Drift via D2
 #'
 #' @description
-#' This function evaluates item parameter drift using the approach described in
-#' Wells et al. (2014). It is designed for assessing item drift in dichotomous
-#' items.
+#' This function assesses item parameter drift using the method outlined in
+#' Wells et al. (2014). It involves comparing the Item Characteristic Curves
+#' (ICCs) of item parameters from two different item pools. This is also
+#' referred to as WRMSD (Weighted Root Mean Squared Difference).
+#'
+#' There are no strict thresholds for determining the significance of D2 in
+#' identifying item drift. A comprehensive approach considering other measures
+#' is recommended. Nevertheless, as a general guideline, for dichotomous items,
+#' a D2 value greater than 0.1 may warrant further scrutiny. For polytomous
+#' items with two thresholds (or three score categories), a D2 value exceeding
+#' 0.15, for those with three thresholds (or four score categories), a D2 value
+#' greater than 0.225, for those with four thresholds (or five score
+#' categories), a D2 value larger than 0.3, and for items with five thresholds
+#' (or six score categories), a D2 value larger than 0.375 may be indicative of
+#' item drift and should be investigated further.
+#'
 #' @param ip1 Itempool object representing the first calibration.
 #' @param ip2 Itempool object representing the second calibration.
 #' @param theta A numeric vector containing the quadrature points.
@@ -15,7 +28,6 @@
 #'   If set to \code{NULL}, it is assumed that all items are considered anchor
 #'   items.
 #'
-
 #' @noRd
 #'
 #' @author Emre Gonulates
@@ -24,6 +36,11 @@
 #' Wells, C. S., Hambleton, R. K., Kirkpatrick, R., & Meng, Y. (2014).
 #' An examination of two procedures for identifying consequential item
 #' parameter drift. Applied Measurement in Education, 27, 214–231.
+#'
+#' Li, Y. (2012), Examining the Impact of Drifted Polytomous Anchor Items on
+#' Test Characteristic Curve (TCC) Linking and IRT True Score Equating. ETS
+#' Research Report Series, 2012: i-22.
+#' https://doi.org/10.1002/j.2333-8504.2012.tb02291.x
 #'
 #' @examples
 #' ip1 <- generate_ip(n = 20)
@@ -55,6 +72,12 @@
 ipd_d2 <- function(ip1, ip2, theta = seq(-4, 4, 0.1),
                    weights = dnorm(seq(-4, 4, 0.1)),
                    anchor_item_ids = NULL) {
+
+  if (!inherits(ip1, "Itempool") || !inherits(ip2, "Itempool"))  {
+    stop("Invalid input for 'ip1' and/or 'ip2'. Both 'ip1' and 'ip2' should ",
+         "be of type 'Itempool'.")
+  }
+
   if (!is_atomic_vector(theta, "numeric")) {
     stop("Invalid 'theta'. 'theta' should be a numeric vector.")
   }
@@ -116,6 +139,182 @@ ipd_d2 <- function(ip1, ip2, theta = seq(-4, 4, 0.1),
 }
 
 
+#' Item Parameter Drift via Robust z
+#'
+#' @description
+#' This function evaluates item parameter drift using the approach described in
+#' Huynh and Meyer (2010).
+#'
+#' @param ip1 An Itempool object for the first calibration.
+#' @param ip2 An Itempool object for the second calibration.
+#' @param anchor_item_ids A character vector containing the IDs of anchor
+#'   items. If set to \code{NULL}, it is assumed that all items are
+#'   considered anchor items.
+#' @param alpha A numeric value ranging from 0 to 1. The two-tailed critical
+#'   value is employed to identify unstable items. For instance, if we calculate
+#'   the critical value using \code{qnorm(1-alpha/2)} (which equals 1.96 when
+#'   \eqn{alpha = 0.05}), items with absolute robust-z values exceeding this
+#'   threshold will be marked as unstable.
+#'
+#' @noRd
+#'
+#' @author Emre Gonulates
+#'
+#' Huynh, Huynh and Meyer, Patrick (2010) "Use of Robust z in Detecting
+#' Unstable Items in Item Response Theory Models,"
+#' \emph{Practical Assessment, Research, and Evaluation}: Vol. 15 , Article 2.
+#' <doi:10.7275/ycx6-e864>
+#'
+#' @examples
+#' ##### Robust z #####
+#' # The example from Huynh and Meyer (2010)
+#' ip1 <- c(itempool(
+#'   a = c(0.729, 0.846, 0.909, 0.818, 0.742, 0.890, 1.741, 0.907, 1.487, 1.228,
+#'         0.672, 1.007, 1.016, 0.776, 0.921, 0.550, 0.624, 0.984, 0.506, 0.594,
+#'         0.687, 0.541, 0.691, 0.843, 0.530, 0.462, 1.007, 0.825, 0.608, 1.177,
+#'         0.900, 0.861, 0.843, 1.404, 0.446, 1.014, 1.632, 0.831, 1.560, 0.798),
+#'   b = c(1.585, 0.635, -0.378, -0.100, -0.195, 0.749, 1.246, 1.016, -0.234,
+#'         0.537, 0.070, 1.985, 1.101, -0.742, 0.463, -0.060, 0.477, 1.084,
+#'         -2.340, 1.068, -0.055, -1.045, 1.859, 0.645, -0.689, -2.583, 1.922,
+#'         0.709, 0.499, 1.973, 0.104, 0.809, 0.640, 0.247, 0.820, 1.837,
+#'         2.129, 1.012, 1.774, 0.095),
+#'   c = c(0.134, 0.304, 0.267, 0.176, 0.215, 0.194, 0.267, 0.159, 0.095,
+#'         0.197, 0.089, 0.272, 0.229, 0.159, 0.162, 0.100, 0.259, 0.167,
+#'         0.000, 0.242, 0.323, 0.000, 0.196, 0.189, 0.000, 0.000, 0.334,
+#'         0.538, 0.125, 0.511, 0.192, 0.353, 0.103, 0.241, 0.245, 0.118,
+#'         0.155, 0.132, 0.215, 0.148),
+#'   model = "3PL"),
+#'   item(a = 0.561, b = c(0.784, -0.113, 1.166), model = "GPCM"),
+#'   item(a = 0.745, b = c(3.687, 2.506, -0.001), model = "GPCM"))
+#'
+#' ip2 <- c(itempool(
+#'   a = c(0.650, 0.782, 0.816, 0.787, 0.611, 0.888, 1.192, 0.589, 1.211,
+#'         0.742, 0.526, 0.690, 0.996, 0.816, 0.781, 0.507, 0.378, 0.976,
+#'         0.473, 0.364, 0.585, 0.566, 0.511, 0.718, 0.354, 1.080, 0.840,
+#'         0.865, 0.528, 0.814, 0.555, 0.701, 0.530, 1.220, 0.344, 0.966,
+#'         1.044, 0.358, 1.192, 0.615),
+#'   b = c(0.676, -0.525, -1.749, -1.092, -1.619, -0.406, -0.132, 0.006,
+#'         -1.352, -0.872, -1.242, 0.873, 0.239, -2.038, -0.487, -1.372,
+#'         -1.492, 0.214, -4.537, 0.220, -0.686, -2.394, 0.747, -0.467,
+#'         -3.629, -5.000, 0.927, 0.305, -0.839, 1.270, -1.618, -0.091,
+#'         -1.228, -1.019, -1.453, 1.090, 1.743, -1.436, 1.024, -1.358),
+#'   c = c(0.110, 0.316, 0.161, 0.149, 0.145, 0.200, 0.243, 0.059, 0.081,
+#'         0.075, 0.028, 0.267, 0.242, 0.189, 0.184, 0.121, 0.000, 0.170,
+#'         0.000, 0.151, 0.383, 0.000, 0.195, 0.177, 0.000, 0.000, 0.352,
+#'         0.647, 0.116, 0.501, 0.000, 0.286, 0.000, 0.248, 0.064, 0.150,
+#'         0.126, 0.000, 0.187, 0.007),
+#'   model = "3PL"),
+#'   item(a = 0.486, b = c(-0.539, -1.489, -0.052), model = "GPCM"),
+#'   item(a = 0.737, b = c(2.599, 1.250, -1.209), model = "GPCM"))
+#' ipd_robustz(ip1, ip2)
+#'
+ipd_robustz <- function(ip1, ip2, anchor_item_ids = NULL, alpha = 0.01) {
+
+  if (!inherits(ip1, "Itempool") || !inherits(ip2, "Itempool"))  {
+    stop("Invalid input for 'ip1' and/or 'ip2'. Both 'ip1' and 'ip2' should ",
+         "be of type 'Itempool'.")
+  }
+
+  # Check if anchor item ids is not null. If it is not, pull those items
+  if (!is.null(anchor_item_ids)) {
+    # Check if anchor_item_ids appeared in both item pools. If not raise an
+    # error.
+    if (!all(anchor_item_ids %in% ip2$resp_id) ||
+        !all(anchor_item_ids %in% ip1$resp_id))
+      stop("All 'anchor_item_ids' should be in both ip2 and ip1.")
+    ip2 <- ip2[anchor_item_ids %in% ip2$resp_id]
+    ip1 <- ip1[anchor_item_ids %in% ip1$resp_id]
+  }
+
+  ip1_df <- as.data.frame(ip1)
+  ip2_df <- as.data.frame(ip2)
+
+
+  # All items are assumed to be anchor items, then
+  # Make sure the item pool sizes and item pool item_ids are matching.
+  if (is.null(anchor_item_ids)) {
+    if (nrow(ip1_df) != nrow(ip2_df)) {
+      stop("Invalid 'anchor_item_ids'. If 'anchor_item_ids' is set to NULL, ",
+           "it means that all items are considered anchor items. In this ",
+           "case, both 'ip1' and 'ip2' should have the same number of items. ",
+           "Please ensure that the sizes of 'ip1' and 'ip2' match."
+           )
+    }
+
+    if (!all(ip1_df$item_id == ip2_df$item_id)) {
+      warning("The item IDs in the item pools do not align. Please ensure ",
+              "that the item IDs and their order match in both item pools. ",
+              "Otherwise, the results may not be accurate.")
+    }
+  }
+
+  # If item models do not match raise a warning
+  if (all(ip1_df$model != ip2_df$model)) {
+    warning("The psychometric models of 'ip1' and 'ip2' do not match. This ",
+            "could potentially lead to inaccurate results.")
+  }
+
+  output <- list()
+  a1 <- ip1_df$a
+  a2 <- ip2_df$a
+  if ((is.null(a2) || any(is.na(a2))) ||
+      (is.null(a1) || any(is.na(a1))))
+    stop("In the item pools, there are some items that do not have 'a' ",
+         "parameters.")
+  a_diff <- log(a2) - log(a1)
+  temp_z <- (a_diff - stats::median(a_diff, na.rm = TRUE)) /
+    (0.74 * stats::IQR(a_diff, na.rm = TRUE))
+  output$a$robust_z <- temp_z
+  output$a$cor <- stats::cor(a2, a1, use = "pairwise.complete.obs")
+  output$a$sd_ratio <- stats::sd(a2, na.rm = TRUE)/stats::sd(a1, na.rm = TRUE)
+
+  cv <- stats::qnorm(1 - alpha/2) # two tailed critical value
+  output$a$unstable <- ip1_df$item_id[!is.na(temp_z) & abs(temp_z) >= cv]
+
+  # Select only stable items to calculate the linking constant
+  A <- exp(mean(a_diff[!ip1_df$item_id %in% output$a$unstable], na.rm = TRUE))
+
+  # ip1_list <- flatten_itempool_cpp(ip1)
+  # ip2_list <- flatten_itempool_cpp(ip2)
+
+  # par <- "d3"
+  for (par in c("b",
+                "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10",
+                "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10"
+                )) {
+    if (!par %in% colnames(ip1_df)) next
+    par1 <- ip1_df[[par]]
+    par2 <- ip2_df[[par]]
+    par_diff <- par1 - A * par2
+    temp_z <- stats::setNames((par_diff - stats::median(par_diff, na.rm = TRUE)) /
+      (0.74 * stats::IQR(par_diff, na.rm = TRUE)), ip1_df$item_id)
+    output[[par]]$robust_z <- temp_z
+    output[[par]]$unstable <- names(temp_z)[!is.na(temp_z) & abs(temp_z) >= cv]
+  }
+
+  # b1 <- b2 <- c()
+  # for (i in seq_along(ip1_list)) {
+  #   temp_b <- ip1_list[[i]]$b
+  #   b1 <- c(b1, stats::setNames(
+  #     temp_b, paste0(ip1_list[[i]]$item_id, if (length(temp_b) > 1)
+  #       paste0(".", 1:length(temp_b)) else NULL)))
+  #
+  #   temp_b <- ip2_list[[i]]$b
+  #   b2 <- c(b2, stats::setNames(
+  #     temp_b, paste0(ip1_list[[i]]$item_id, if (length(temp_b) > 1)
+  #       paste0(".", 1:length(temp_b)) else NULL)))
+  #
+  # }
+  # b_diff <- b1 - A * b2
+  # output$b$robust_z <- (b_diff - stats::median(b_diff)) /
+  #   (0.74 * stats::IQR(b_diff))
+  # output$b$unstable <- names(output$b$robust_z)[output$b$robust_z >= cv]
+
+  return(output)
+}
+
+
+
 #' Item Parameter Drift
 #'
 #' @description
@@ -127,15 +326,39 @@ ipd_d2 <- function(ip1, ip2, theta = seq(-4, 4, 0.1),
 #' @param method The method for analyzing item parameter drift.
 #'   \describe{
 #'     \item{"robust-z"}{Robust-Z method based on the Huynh and Meyer (2010).}
+#'     \item{"d2"}{D2 method assesses item parameter drift using the method
+#'       outlined in Wells et al. (2014). It involves comparing the Item
+#'       Characteristic Curves (ICCs) of item parameters from two different
+#'       item pools. This is also referred to as WRMSD (Weighted Root Mean
+#'       Squared Difference).
+#'
+#'       There are no strict thresholds for determining the significance of D2
+#'       in identifying item drift. A comprehensive approach considering other
+#'       measures is recommended. Nevertheless, as a general guideline, for
+#'       dichotomous items, a D2 value greater than 0.1 may warrant further
+#'       scrutiny. For polytomous items with two thresholds (or three score
+#'       categories), a D2 value exceeding 0.15, for those with three thresholds
+#'       (or four score categories), a D2 value greater than 0.225, for those
+#'       with four thresholds (or five score categories), a D2 value larger than
+#'       0.3, and for items with five thresholds (or six score categories), a D2
+#'       value larger than 0.375 may be indicative of item drift and should be
+#'       investigated further.
+#'     }
 #'   }
 #' @param anchor_item_ids A character vector containing the IDs of anchor
 #'   items. If set to \code{NULL}, it is assumed that all items are
 #'   considered anchor items.
-#' @param alpha A numeric value ranging from 0 to 1. The two-tailed critical
-#'   value is employed to identify unstable items. For instance, if we calculate
-#'   the critical value using \code{qnorm(1-alpha/2)} (which equals 1.96 when
-#'   \eqn{alpha = 0.05}), items with absolute robust-z values exceeding this
-#'   threshold will be marked as unstable.
+#' @param alpha A numeric value ranging from 0 to 1. Only needed when
+#'   \code{method = "robust-z"}. The two-tailed critical value is employed to
+#'   identify unstable items. For instance, if we calculate the critical value
+#'   using \code{qnorm(1-alpha/2)} (which equals 1.96 when \eqn{alpha = 0.05}),
+#'   items with absolute robust-z values exceeding this threshold will be marked
+#'   as unstable.
+#' @param theta A numeric vector containing the quadrature points. Only needed
+#'   when \code{method = "d2"}.
+#' @param weights A numeric vector containing the weights assigned to the
+#'   quadrature points. The length of this vector should match the length of the
+#'   \code{theta} argument. Only needed when \code{method = "d2"}
 #'
 #'
 #' @return Return a list depending on the method:
@@ -162,6 +385,9 @@ ipd_d2 <- function(ip1, ip2, theta = seq(-4, 4, 0.1),
 #'           (i.e., \code{qnorm(1-alpha/2)}).}
 #'       }
 #'     }
+#'     \item{d2}{A numeric vector containing the differences between the ICCs
+#'       of each item.
+#'     }
 #'   }
 #'
 #'
@@ -176,6 +402,7 @@ ipd_d2 <- function(ip1, ip2, theta = seq(-4, 4, 0.1),
 #' <doi:10.7275/ycx6-e864>
 #'
 #' @examples
+#' ##### Robust-z #####
 #' # The example from Huynh and Meyer (2010)
 #' ip1 <- c(itempool(
 #'   a = c(0.729, 0.846, 0.909, 0.818, 0.742, 0.890, 1.741, 0.907, 1.487, 1.228,
@@ -217,60 +444,46 @@ ipd_d2 <- function(ip1, ip2, theta = seq(-4, 4, 0.1),
 #'   item(a = 0.737, b = c(2.599, 1.250, -1.209), model = "GPCM"))
 #' ipd(ip1, ip2)
 #'
+#' ##### D2 #####
+#' ip1 <- generate_ip(n = 20)
+#' ip2 <- ip1
+#' # add a small nuisance to item difficulty parameters
+#' ip2$b <- ip1$b + runif(20, -.5, .5)
+#'
+#' theta <- seq(-4, 4, 0.2)
+#' weights <- dnorm(theta)
+#' ipd(ip1, ip2, theta = theta, weights = weights)
+#'
+#' # Calculate for only certain items
+#' ipd(ip1, ip2, theta = theta, weights = weights,
+#'   anchor_item_ids = c("Item_2", "Item_6", "Item_9", "Item_13"))
+#'
+#' ### Polytomous items items
+#' n_item <- 30
+#' models <- sample(c("3PL", "GPCM2"), n_item, TRUE)
+#' new_ip <- generate_ip(model = models, D = 1.702)
+#' old_ip <- data.frame(new_ip)
+#' old_ip$a <- old_ip$a + round(runif(n_item, min = -.5, max = .5), 2)
+#' old_ip$b <- old_ip$b + round(runif(n_item, min = -.75, max = .75), 2)
+#' old_ip$d1 <- old_ip$d1 + round(runif(n_item, min = -.75, max = .75), 2)
+#' old_ip$d2 <- old_ip$d2 + round(runif(n_item, min = -.75, max = .75), 2)
+#' old_ip$d3 <- old_ip$d3 + round(runif(n_item, min = -.75, max = .75), 2)
+#' old_ip <- itempool(old_ip)
+#'
+#' ipd(ip1 = old_ip, ip2 = new_ip, theta = theta, weights = weights)
+#'
 ipd <- function(ip1, ip2, method = "robust-z", anchor_item_ids = NULL,
-                alpha = 0.01) {
+                alpha = 0.01,
+                theta = seq(-4, 4, 0.1), weights = dnorm(seq(-4, 4, 0.1))
+                ) {
   if (method == "robust-z") {
-    # Check if anchor item ids is not null. If it is not, pull those items
-    if (!is.null(anchor_item_ids)) {
-      # Check if anchor_item_ids appeared in both item pools. If not raise an
-      # error.
-      if (!all(anchor_item_ids %in% ip2$resp_id) ||
-          !all(anchor_item_ids %in% ip1$resp_id))
-        stop("All 'anchor_item_ids' should be in both ip2 and ip1.")
-      ip2 <- ip2[anchor_item_ids %in% ip2$resp_id]
-      ip1 <- ip1[anchor_item_ids %in% ip1$resp_id]
-    }
-
-    output <- list()
-    a2 <- ip2$a
-    a1 <- ip1$a
-    if ((is.null(a2) || any(is.na(a2))) ||
-        (is.null(a1) || any(is.na(a1))))
-      stop("In the item pools, there are some items that do not have 'a' ",
-           "parameters.")
-    a_diff <- log(a2) - log(a1)
-    output$a$robust_z <- (a_diff - stats::median(a_diff)) /
-      (0.74 * stats::IQR(a_diff))
-    output$a$cor <- stats::cor(a2, a1)
-    output$a$sd_ratio <- stats::sd(a2)/stats::sd(a1)
-
-    cv <- stats::qnorm(1 - alpha/2) # two tailed critical value
-    output$a$unstable <- ip1$item_id[abs(output$a$robust_z) >= cv]
-
-    # Select only stable items to calculate the linking constant
-    A <- exp(mean(a_diff[abs(output$a$robust_z) < cv]))
-
-    ip1_list <- flatten_itempool_cpp(ip1)
-    ip2_list <- flatten_itempool_cpp(ip2)
-
-    b1 <- b2 <- c()
-    for (i in seq_along(ip1_list)) {
-      temp_b <- ip1_list[[i]]$b
-      b1 <- c(b1, stats::setNames(
-        temp_b, paste0(ip1_list[[i]]$item_id, if (length(temp_b) > 1)
-          paste0(".", 1:length(temp_b)) else NULL)))
-
-      temp_b <- ip2_list[[i]]$b
-      b2 <- c(b2, stats::setNames(
-        temp_b, paste0(ip1_list[[i]]$item_id, if (length(temp_b) > 1)
-          paste0(".", 1:length(temp_b)) else NULL)))
-
-    }
-    b_diff <- b1 - A * b2
-    output$b$robust_z <- (b_diff - stats::median(b_diff)) /
-      (0.74 * stats::IQR(b_diff))
-    output$b$unstable <- names(output$b$robust_z)[output$b$robust_z >= cv]
-    return(output)
+    output <- ipd_robustz(ip1 = ip1, ip2 = ip2,
+                          anchor_item_ids = anchor_item_ids, alpha = alpha)
+  } else if (method == "d2") {
+    output <- ipd_d2(ip1 = ip1, ip2 = ip2,
+                     anchor_item_ids = anchor_item_ids, theta = theta,
+                     weights = weights)
   } else
     stop("This method has not been implemented yet.", call. = FALSE)
+  return(output)
 }
--- tests/testthat/test-ipd.R
+++ tests/testthat/test-ipd.R
@@ -1,14 +1,140 @@
 
-test_that("ipd", {
+test_that("ipd_robustz", {
+
+  # -------------------------------------------------------------------------- #
+  # Error raised when item pools have different number of items
+  n_item <- sample(10:20, 1)
+  ip1 <- generate_ip(n = n_item)
+  ip2 <- generate_ip(n = n_item + 1)
+  expect_error(ipd(ip1 = ip1, ip2 = ip2, method = "robust-z"),
+               "Invalid 'anchor_item_ids'")
+
+  # -------------------------------------------------------------------------- #
+  # Warning raised when item pools have different item IDs
+  n_item <- sample(10:20, 1)
+  ip1 <- generate_ip(n = n_item)
+  ip2 <- generate_ip(n = n_item)
+  ip2$item_id <- sample(ip1$item_id)
+  expect_warning(ipd(ip1 = ip1, ip2 = ip2, method = "robust-z"),
+                 "The item IDs in the item pools do not align." )
+
+  # -------------------------------------------------------------------------- #
+  # Robust-z
   n <- 20
   ip1 <- generate_ip(n = n)
   ipdf1 <- ipdf2 <- as.data.frame(ip1)
   ipdf2$a <- ipdf1$a + runif(n, -0.25, .25)
   ipdf2$b <- ipdf1$b + runif(n, -0.25, .25)
-  ipdf2$a[1] <- ipdf1$a[1] + 1
+  ipdf2$a[1] <- ipdf1$a[1] + 2
   ipdf2$b[n] <- ipdf1$b[n] - 1
   ip2 <- itempool(ipdf2)
-  result <- ipd(ip1 = ip1, ip2 = ip2)
+  result <- ipd(ip1 = ip1, ip2 = ip2, method = "robust-z")
   expect_true("Item_1" %in% result$a$unstable)
   expect_true(paste0("Item_", n) %in% result$b$unstable)
+
+
+
+  # -------------------------------------------------------------------------- #
+  # Robust-z
+  # The example from Huynh and Meyer (2010)
+  ip1 <- c(itempool(
+    a = c(0.729, 0.846, 0.909, 0.818, 0.742, 0.890, 1.741, 0.907, 1.487, 1.228,
+          0.672, 1.007, 1.016, 0.776, 0.921, 0.550, 0.624, 0.984, 0.506, 0.594,
+          0.687, 0.541, 0.691, 0.843, 0.530, 0.462, 1.007, 0.825, 0.608, 1.177,
+          0.900, 0.861, 0.843, 1.404, 0.446, 1.014, 1.632, 0.831, 1.560, 0.798),
+    b = c(1.585, 0.635, -0.378, -0.100, -0.195, 0.749, 1.246, 1.016, -0.234,
+          0.537, 0.070, 1.985, 1.101, -0.742, 0.463, -0.060, 0.477, 1.084,
+          -2.340, 1.068, -0.055, -1.045, 1.859, 0.645, -0.689, -2.583, 1.922,
+          0.709, 0.499, 1.973, 0.104, 0.809, 0.640, 0.247, 0.820, 1.837,
+          2.129, 1.012, 1.774, 0.095),
+    c = c(0.134, 0.304, 0.267, 0.176, 0.215, 0.194, 0.267, 0.159, 0.095,
+          0.197, 0.089, 0.272, 0.229, 0.159, 0.162, 0.100, 0.259, 0.167,
+          0.000, 0.242, 0.323, 0.000, 0.196, 0.189, 0.000, 0.000, 0.334,
+          0.538, 0.125, 0.511, 0.192, 0.353, 0.103, 0.241, 0.245, 0.118,
+          0.155, 0.132, 0.215, 0.148),
+    model = "3PL"),
+    item(a = 0.561, b = c(0.784, -0.113, 1.166), model = "GPCM"),
+    item(a = 0.745, b = c(3.687, 2.506, -0.001), model = "GPCM"))
+
+  ip2 <- c(itempool(
+    a = c(0.650, 0.782, 0.816, 0.787, 0.611, 0.888, 1.192, 0.589, 1.211,
+          0.742, 0.526, 0.690, 0.996, 0.816, 0.781, 0.507, 0.378, 0.976,
+          0.473, 0.364, 0.585, 0.566, 0.511, 0.718, 0.354, 1.080, 0.840,
+          0.865, 0.528, 0.814, 0.555, 0.701, 0.530, 1.220, 0.344, 0.966,
+          1.044, 0.358, 1.192, 0.615),
+    b = c(0.676, -0.525, -1.749, -1.092, -1.619, -0.406, -0.132, 0.006,
+          -1.352, -0.872, -1.242, 0.873, 0.239, -2.038, -0.487, -1.372,
+          -1.492, 0.214, -4.537, 0.220, -0.686, -2.394, 0.747, -0.467,
+          -3.629, -5.000, 0.927, 0.305, -0.839, 1.270, -1.618, -0.091,
+          -1.228, -1.019, -1.453, 1.090, 1.743, -1.436, 1.024, -1.358),
+    c = c(0.110, 0.316, 0.161, 0.149, 0.145, 0.200, 0.243, 0.059, 0.081,
+          0.075, 0.028, 0.267, 0.242, 0.189, 0.184, 0.121, 0.000, 0.170,
+          0.000, 0.151, 0.383, 0.000, 0.195, 0.177, 0.000, 0.000, 0.352,
+          0.647, 0.116, 0.501, 0.000, 0.286, 0.000, 0.248, 0.064, 0.150,
+          0.126, 0.000, 0.187, 0.007),
+    model = "3PL"),
+    item(a = 0.486, b = c(-0.539, -1.489, -0.052), model = "GPCM"),
+    item(a = 0.737, b = c(2.599, 1.250, -1.209), model = "GPCM"))
+  rz <- ipd(ip1, ip2, method = "robust-z")
+  expect_true(all(rz$a$robust_z > -10 & rz$a$robust_z < 10, na.rm = TRUE))
+  expect_true(all(rz$b$robust_z > -10 & rz$b$robust_z < 10, na.rm = TRUE))
+
+
+  # -------------------------------------------------------------------------- #
+  # Robust-z example with GPCM2 and 2PL items
+  n_item <- 20
+  ip1 <- generate_ip(model = sample(c("GPCM2", "2PL"), n_item, T),
+                     n_categories = sample(3:5, n_item, T))
+  ip2 <- as.data.frame(ip1)
+  # Add some noise to item parameters
+  ip2$a <- ip2$a + runif(n_item, -0.25, .25)
+  ip2$b <- ip2$b + runif(n_item, -0.5, .5)
+  ip2$d1 <- ip2$d1 + runif(n_item, -0.25, .25)
+  ip2$d2 <- ip2$d2 + runif(n_item, -0.25, .25)
+  ip2$d3 <- ip2$d3 + runif(n_item, -0.25, .25)
+  ip2$d4 <- ip2$d4 + runif(n_item, -0.25, .25)
+  ip2 <- itempool(ip2)
+
+  rz <- ipd(ip1, ip2, method = "robust-z")
+  expect_true(all(rz$a$robust_z > -10 & rz$a$robust_z < 10, na.rm = TRUE))
+  expect_true(all(rz$d1$robust_z > -10 & rz$d1$robust_z < 10, na.rm = TRUE))
+
+  # -------------------------------------------------------------------------- #
+  # Robust-z example with GPCM and 2PL items
+  n_item <- 20
+  ip1 <- generate_ip(model = sample(c("GPCM", "2PL"), n_item, T),
+                     n_categories = sample(3:5, n_item, T))
+  ip2 <- as.data.frame(ip1)
+  # Add some noise to item parameters
+  ip2$a <- ip2$a + runif(n_item, -0.25, .25)
+  ip2$b1 <- ip2$b1 + runif(n_item, -0.25, .25)
+  ip2$b2 <- ip2$b2 + runif(n_item, -0.25, .25)
+  ip2$b3 <- ip2$b3 + runif(n_item, -0.25, .25)
+  ip2$b4 <- ip2$b4 + runif(n_item, -0.25, .25)
+  ip2 <- itempool(ip2)
+
+  rz <- ipd(ip1, ip2, method = "robust-z")
+  expect_true(all(rz$a$robust_z > -10 & rz$a$robust_z < 10, na.rm = TRUE))
+  expect_true(all(rz$b1$robust_z > -10 & rz$b1$robust_z < 10, na.rm = TRUE))
+
+
+  # -------------------------------------------------------------------------- #
+  # Robust-z example with GRM and 2PL items
+  n_item <- 20
+  ip1 <- generate_ip(model = sample(c("GRM", "2PL"), n_item, T),
+                     n_categories = sample(3:5, n_item, T))
+  ip2 <- as.data.frame(ip1)
+  # Add some noise to item parameters
+  ip2$a <- ip2$a + runif(n_item, -0.25, .25)
+  ip2$b1 <- ip2$b1 + runif(n_item, -0.25, .25)
+  ip2$b2 <- ip2$b2 + runif(n_item, -0.25, .25)
+  ip2$b3 <- ip2$b3 + runif(n_item, -0.25, .25)
+  ip2$b4 <- ip2$b4 + runif(n_item, -0.25, .25)
+  ip2 <- itempool(ip2)
+
+  rz <- ipd(ip1, ip2, method = "robust-z")
+  expect_true(all(rz$a$robust_z > -10 & rz$a$robust_z < 10, na.rm = TRUE))
+  expect_true(all(rz$b1$robust_z > -10 & rz$b1$robust_z < 10, na.rm = TRUE))
+
 })
+
